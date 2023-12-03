0823d37d91778b48ea7e4828753161dbaabd0823
PulsarBITW <GordeyBITW@gmail.com>
2023-12-04 02:33:39 +0300

    ы

--- app/layout.js
+++ app/layout.js
@@ -7,22 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Try Next",
+  description: "try next - a new project",
 };
 
 const status = !false; // for checking global-error.jsx
 
 export default function RootLayout({ children }) {
-  if (!status) throw new Error("global error");
-
   return (
     <html lang="en">
-      <head>
-        <meta property="og:image" content="/1111.png" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="438" />
-        <meta property="og:description" content="Описание для try next" />
-      </head>
       <body className={inter.className}>
         <Providers>
           <div className="wrapper">
